a0ab80703396de30e83c5ed7f21a6d89167907e9
lxg <1436215718@qq.com>
2019-03-27 22:01:27 +0800

    添加项目文件。

---
+++ AspEFCore.Data/MyContext.cs
@@ -0,0 +1,32 @@
+﻿using AspEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspEFCore.Data
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>().HasKey(x => x.Id);
+            modelBuilder.Entity<Company>().HasKey(x => x.Id);
+            modelBuilder.Entity<Province>().HasKey(x => x.Id);
+            modelBuilder.Entity<City>().HasOne(x => x.Province).WithMany(x => x.Cities).HasForeignKey(x => x.ProvinceId);
+            modelBuilder.Entity<CityCompany>().HasKey(x => new { x.CityId, x.CompanyId });
+            modelBuilder.Entity<CityCompany>().HasOne(x => x.City).WithMany(x => x.CityCompanies).HasForeignKey(x => x.CityId);
+            modelBuilder.Entity<CityCompany>().HasOne(x => x.Company).WithMany(x => x.CityCompanies).HasForeignKey(x => x.CompanyId);
+            modelBuilder.Entity<Mayor>().HasKey(x => x.Id);
+            modelBuilder.Entity<Mayor>().HasOne(x => x.City).WithOne(x => x.Mayor).HasForeignKey<Mayor>(x => x.CityId);
+        }
+        public DbSet<Province> Province { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<CityCompany> CityCompanies { get; set; }
+        public DbSet<Mayor> Mayors { get; set; }
+
+    }
+}
---
+++ AspEFCore.Web/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspEFCore.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspEFCore.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MyContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
